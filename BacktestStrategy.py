a86c069890fc943e2a23d02e7649a2868ca9c3f8
j1729s <105176950+j1729s@users.noreply.github.com>
2023-04-19 20:43:47 +0530

    Update BacktestStrategy.py

--- BacktestStrategy.py
+++ BacktestStrategy.py
@@ -7,11 +7,12 @@ from BuildLinearModel import build_linear_model
 warnings.filterwarnings("ignore")
 
 
-def backtest_strategy(train_data, test_data, threshold=0.2, l=5):
+def backtest_strategy(train_data, test_data, to_test='Pred', threshold=0.2, l=5):
     """
     Backtests the strategy and prints out the metrics
     :param train_data: Training Dataset
     :param test_data: Test data for prediction
+    :param to_test: Backtest with real or predicted data
     :param l: the no. of LAGS for VOI and OIR determined by the ACF Plot
     :param threshold: trading threshold
     :return: dataframe with Price, Predicted MPC, and True MPC as columns
@@ -28,7 +29,7 @@ def backtest_strategy(train_data, test_data, threshold=0.2, l=5):
     y_pred = model.predict(sm.add_constant(df))
     y_pred = np.where(y_pred > threshold, 1, np.where(y_pred < -threshold, -1, 0))
     test_data["MPC_pred"] = y_pred
-    y_true = np.where(test_data["MPC"] > threshold, 1, np.where(test_data["MPC"] < -threshold, -1, 0))
+    y_true = pd.Series(np.where(test_data["MPC"] > threshold, 1, np.where(test_data["MPC"] < -threshold, -1, 0)), index=test_data.index)
     df = test_data[["Price", "MPC_pred"]]
     df["MPC"] = y_true
     
@@ -40,10 +41,16 @@ def backtest_strategy(train_data, test_data, threshold=0.2, l=5):
     t_cost = 0
     t_volume = 0
     
-    for index in df.index:
+    if to_test == 'Pred':
+        data = df['MPC_pred']
+    
+    elif to_test == 'Real':
+        data = df['MPC']
+    
+    for index in data.index:
         
         # BUY to OPEN
-        if own == False and df.loc[index, "MPC_pred"] == 1:
+        if own == False and data.loc[index] == 1:
             
             own = True
             position = 1
@@ -53,7 +60,7 @@ def backtest_strategy(train_data, test_data, threshold=0.2, l=5):
             t_volume += 1
             
         # SELL to OPEN    
-        elif own == False and df.loc[index, "MPC_pred"] == -1:
+        elif own == False and data.loc[index] == -1:
            
             own = True
             position = -1
@@ -63,7 +70,7 @@ def backtest_strategy(train_data, test_data, threshold=0.2, l=5):
             t_volume += 1
             
         # SELL to TRADE
-        elif own and position == 1 and df.loc[index, "MPC_pred"] == -1:
+        elif own and position == 1 and data.loc[index] == -1:
             
             own == True
             position = -1
@@ -74,7 +81,7 @@ def backtest_strategy(train_data, test_data, threshold=0.2, l=5):
             t_volume += 2
             
         # BUY to TRADE    
-        elif own and position == -1 and df.loc[index, "MPC_pred"] == 1:
+        elif own and position == -1 and data.loc[index] == 1:
             
             own == True
             position = 1
@@ -85,7 +92,7 @@ def backtest_strategy(train_data, test_data, threshold=0.2, l=5):
             t_volume += 2
         
         # CLOSE at day end
-        elif position == 1 and index == df.index[-1]:
+        elif position == 1 and index == data.index[-1]:
             
             price = df.loc[index, "Price"]
             cost.append(price)
@@ -93,13 +100,15 @@ def backtest_strategy(train_data, test_data, threshold=0.2, l=5):
             t_volume += 1
         
         # CLOSE at day end
-        elif position == -1 and index == df.index[-1]:
+        elif position == -1 and index == data.index[-1]:
             
             price = df.loc[index, "Price"]
             cost.append(-1*price)
             t_cost += TC*price
             t_volume += 1
-            
+    
+    print("Profit before transaction cost = {} USD".format(sum(cost)))
+    print("Transaction Cost = {} USD".format(t_cost))
     print("Total Profit = {} USD".format(sum(cost)-t_cost))
     print("Total Trade Volume = {} trades".format(t_volume))
     
@@ -114,6 +123,7 @@ if __name__ == '__main__':
     # Add arguments
     parser.add_argument('train_file', help='Path to training dataset CSV file')
     parser.add_argument('test_file', help='Path to test dataset CSV file')
+    parser.add_argument('--to_test', type=str, default='Pred', help='Backtest with real or predicted data')
     parser.add_argument('--threshold', type=float, default=0.2, help='Trading threshold (default=0.2)')
     parser.add_argument('--lags', type=int, default=5, help='The no. of LAGS for VOI and OIR determined by the ACF Plot (default=5)')
     
@@ -125,4 +135,4 @@ if __name__ == '__main__':
     test = pd.read_csv(args.test_file)
     
     # Call the function with the parsed arguments
-    backtest_strategy(train, test, args.threshold, args.lags)
+    backtest_strategy(train, test, args.to_test, args.threshold, args.lags)
